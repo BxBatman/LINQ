e2465588d1d448021cc1604d7f408093f93b179c
Bartek <bartekwysocki46@gmail.com>
2018-05-26 18:08:33 +0200

    Dodaj pliki projektów.

---
+++ LINQ/FileReader.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace LINQ
+{
+    class FileReader
+    {
+        private String path;
+        private XElement xElement;
+        private IEnumerable<XElement> books;
+
+        public FileReader()
+        {
+            this.path = "../../books.xml";
+            this.xElement = XElement.Load(path);
+            this.books = xElement.Elements();
+        }
+        
+
+        public void searchById(string id)
+        {
+            var idList = from idl in xElement.Elements("book")
+                         where (string)idl.Attribute("id") == id
+                         select idl;
+
+            foreach (XElement xEle in idList)
+            {
+                Console.WriteLine("Autor: " + xEle.Element("author").Value);
+                Console.WriteLine("Tytuł: " + xEle.Element("title").Value);
+                Console.WriteLine("Kategoria: "+xEle.Element("genre").Value);
+                Console.WriteLine("Cena: "+xEle.Element("price").Value);
+                Console.WriteLine("Data publikacji: "+xEle.Element("publish_date").Value);
+                Console.WriteLine("Opis: "+xEle.Element("description").Value);
+
+            }
+
+        }
+
+        public void searchByPrice(double price)
+        {
+            var idPrice = from idp in xElement.Elements("book")
+                          where (double)idp.Element("price") > price
+                          select idp;
+
+            foreach(XElement xEle in idPrice)
+            {
+                Console.WriteLine("Autor: " + xEle.Element("author").Value);
+                Console.WriteLine("Tytuł: " + xEle.Element("title").Value);
+                Console.WriteLine("Cena: " + xEle.Element("price").Value);
+                
+            }
+        }
+
+        public void searchByGenre(string genre)
+        {
+            var idGenre = from idg in xElement.Elements("book")
+                          where (string)idg.Element("genre") == genre
+                          select idg;
+
+            foreach(XElement xEle in idGenre)
+            {
+                Console.WriteLine("Autor: " + xEle.Element("author").Value);
+                Console.WriteLine("Tytuł: " + xEle.Element("title").Value);
+                Console.WriteLine("Kategoria: " + xEle.Element("genre").Value);
+                Console.WriteLine("Cena: " + xEle.Element("price").Value);
+            }
+        }
+    }
+}
---
+++ LINQ/Menu.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ
+{
+    class Menu
+    { FileReader fileReader;
+        public void showMenu()
+        {
+            fileReader = new FileReader();
+            Console.WriteLine("------MENU------");
+            Console.WriteLine("Wyszukaj po: ");
+            Console.WriteLine("1.Id - dane o książce");
+            Console.WriteLine("2.Cena - większa od");
+            Console.WriteLine("3.Kategoria - dane o książce");
+            int choose = Convert.ToInt32(Console.ReadLine());
+            chooseExecute(choose);
+
+            
+        }
+
+        private void chooseExecute(int choose)
+        {
+            Console.WriteLine("Podaj wartość");
+            switch (choose)
+            {
+                case 1:
+                    string id = Console.ReadLine();
+                    
+                    fileReader.searchById(id);
+                    break;
+                case 2:
+                    double price = Convert.ToDouble(Console.ReadLine());
+                    fileReader.searchByPrice(price);
+                    break;
+                case 3:
+                    string cat = Console.ReadLine();
+                    fileReader.searchByGenre(cat);
+                    break;
+                        
+            }
+            
+        }
+    }
+}
